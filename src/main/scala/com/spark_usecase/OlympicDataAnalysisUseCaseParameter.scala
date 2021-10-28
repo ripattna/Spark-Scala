8b3fd602a3bcbaa1b1ca3acf1172a4fc6076d686
ripattna <ripattna@gmail.com>
2021-10-28 16:56:39 +0530

    Code Change

--- src/main/scala/com/spark_usecase/OlympicDataAnalysisUseCaseParameter.scala
+++ src/main/scala/com/spark_usecase/OlympicDataAnalysisUseCaseParameter.scala
@@ -1,6 +1,5 @@
 package com.spark_usecase
 
-import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.sql.SparkSession
 
@@ -10,45 +9,45 @@ object OlympicDataAnalysisUseCaseParameter {
 
     if(args.length==3) {
 
-    // Creating SparkSession
-    val spark = SparkSession.builder()
-      .master(args(0))
-      .appName("OlympicData")
-      .getOrCreate()
+      // Creating SparkSession
+      val spark = SparkSession.builder()
+        .master(args(0))
+        .appName("OlympicData")
+        .getOrCreate()
 
-    // Creating log level
-    spark.sparkContext.setLogLevel("WARN")
+      // Creating log level
+      spark.sparkContext.setLogLevel("WARN")
 
-    val textFile = spark.sparkContext.textFile(args(1))
-    val counts =  textFile.filter{
-      x => {
-        if(x.toString().split("\t").length >=10)
-          true
-        else
-          false
-      }
-    }.map(line=>{line.toString().split("\t")})
-    //counts.take(10).foreach(println)
+      val textFile = spark.sparkContext.textFile(args(1))
+      val counts =  textFile.filter{
+        x => {
+          if(x.toString().split("\t").length >=10)
+            true
+          else
+            false
+        }
+      }.map(line=>{line.toString().split("\t")})
+      //counts.take(10).foreach(println)
 
-    val fil = counts.filter(
-      x => {
-        if(x(5).equalsIgnoreCase("swimming")&&(x(9).matches(("\\d+"))))
-          true
-        else
-          false
-      }
-    )
-    //println(fil.count())
+      val fil = counts.filter(
+        x => {
+          if(x(5).equalsIgnoreCase("swimming")&&(x(9).matches(("\\d+"))))
+            true
+          else
+            false
+        }
+      )
+      //println(fil.count())
 
-    val pairs: RDD[(String,Int)] = fil.map(x => (x(2),x(9).toInt))
-    val cnt = pairs.reduceByKey(_+_).saveAsTextFile(args(2))
-    //cnt.take(20).foreach(println)
+      val pairs: RDD[(String,Int)] = fil.map(x => (x(2),x(9).toInt))
+      val cnt = pairs.reduceByKey(_+_).saveAsTextFile(args(2))
+      //cnt.take(20).foreach(println)
 
-     }
+    }
     else
-      {
-        println("Enter the three arguments")
-      }
+    {
+      println("Enter the three arguments")
+    }
 
   }
 }
