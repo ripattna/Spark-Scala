007699accde355340e79e7d11e5702300c28d305
ripattna <ripattna@gmail.com>
2021-11-24 20:49:54 +0530

    Code Change

--- src/main/scala/com/dataframe/Csv_Operation.scala
+++ src/main/scala/com/dataframe/Csv_Operation.scala
@@ -3,6 +3,7 @@ package com.dataframe
 import org.apache.spark.sql.SparkSession
 
 object Csv_Operation {
+
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder().appName("Spark Hive Example").master("local[1]").getOrCreate()
@@ -10,9 +11,16 @@ object Csv_Operation {
     // Creating log level
     spark.sparkContext.setLogLevel("WARN")
 
-    val path = "C:/Project/Files/Input/csv/Sample.csv"
-    val df = spark.read.option("header", "true").csv(path)
-    // df.take(10).foreach(println)
+    //val path = "src/main/resources/western.csv"
+    //val df3 = spark.read.option("header", "true").csv(path)
+
+    val df = spark.read.format("csv")
+      .option("sep", ",")
+      .option("inferSchema", "true")
+      .option("header", true)
+      .load("src/main/resources/northern.csv")
+
+    df.take(10).foreach(println)
 
     df.printSchema()
     // df.show()
---
+++ src/main/scala/com/dataframe/UnionOperation.scala
@@ -0,0 +1,44 @@
+package com.dataframe
+
+import org.apache.spark.sql.SparkSession
+
+object UnionOperation {
+
+  def main(args:  Array[String]): Unit = {
+
+    // Create SparkSession
+    val spark: SparkSession = SparkSession.builder().master("local[1]").appName("UnionOperation").getOrCreate()
+
+    // Creating log level
+    spark.sparkContext.setLogLevel("WARN")
+
+    // Step-1: Reading the first dataset
+    val northDF = spark.read.format("csv")
+      .option("sep", ",")
+      .option("inferSchema", "true")
+      .option("path","src/main/resources/northern.csv")
+      .load()
+      .toDF("state","capital")
+
+    // Step-2: Show the 1st loaded input file
+    northDF.show()
+
+    // Step-3: Load the 2nd input file
+    val westDF = spark.read.format("csv")
+      .option("sep", ",")
+      .option("path","src/main/resources/western.csv")
+      .load()
+      .toDF("state","capital")
+
+
+    // Step-4: Show the 2nd loaded input file
+    westDF.show()
+
+    // Step-5: Combining both dataframes, sorting & showing results
+    northDF.union(westDF)
+      .sort(("state"))
+      .show(false)
+
+
+  }
+}
