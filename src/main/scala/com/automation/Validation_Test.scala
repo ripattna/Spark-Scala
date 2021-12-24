b4ec17ee8ac2344fcb67c11c25e2a07804f077f0
ripattna <ripattna@gmail.com>
2021-12-24 17:35:34 +0530

    Code Change

--- src/main/scala/com/automation/Validation_Test.scala
+++ src/main/scala/com/automation/Validation_Test.scala
@@ -15,9 +15,10 @@ object Validation_Test {
 
     // Read the source file
     val df1 = spark.read.option("header", "true").option("inferSchema", "true").csv("C:\\Project\\Files\\df1.csv")
+    // df1.show()
     // Read the target file
     val df2 = spark.read.option("header", "true").option("inferSchema", "true").csv("C:\\Project\\Files\\df2.csv")
-
+    // df2.show()
     println("For No of Rows in Source where value is 1")
     // For No of Rows in Source where value is 1
     val source_data = df1.agg(sum("valueA"), sum("valueB"), sum("valueC"),
@@ -52,10 +53,48 @@ object Validation_Test {
     val join_data = b.join(b1, b("Column_Name") === b1("Column_Name"), "inner").drop(b1("Column_Name"))
     join_data.show()
 
-    df2.except(df1).show(false)
+    val join_data1 = df1.join(df2, df1("Primary_Key") === df2("Primary_Key"), "inner").drop(df2("Primary_Key"))
+    println("Inner join result")
+    //println(join_data1.count())
 
-    println("Other Table")
-    df1.except(df2).show(false)
+    // Using Join with multiple columns on where clause
+    println("Join result for valueA")
+    val valueA = df1.join(df2).where(df1("Primary_Key") === df2("Primary_Key") &&
+      df1("valueA") === df2("valueA"))
+    println(valueA.count())
+
+    println("Join result for valueB")
+    val valueB = df1.join(df2).where(df1("Primary_Key") === df2("Primary_Key") &&
+      df1("valueB") === df2("valueB"))
+    println(valueB.count())
+
+    println("Join result for valueC")
+    val valueC = df1.join(df2).where(df1("Primary_Key") === df2("Primary_Key") &&
+      df1("valueC") === df2("valueC"))
+    println(valueC.count())
+
+    println("Join result for valueD")
+    val valueD = df1.join(df2).where(df1("Primary_Key") === df2("Primary_Key") &&
+      df1("valueD") === df2("valueD"))
+    println(valueD.count())
+
+    // Left Anti Join
+    println("Left Anti join")
+    val valueA_Anti = df1.join(df2).where(df1("Primary_Key") === df2("Primary_Key") && df1("valueA") === df2("valueA"))
+    println(valueA_Anti.count())
+
+    df1.join(df2,df1("Primary_Key") ===  df2("Primary_Key"),"left_anti").show(false)
+
+    println("New Left Join Result")
+    df1.join(df2,df1("Primary_Key") === df2("Primary_Key") &&  df1("valueA") === df2("valueA"), "left_anti").show()
+
+    //  println("Present in Source but not present in target")
+
+    // df1.except(df2).show(false)
+
+    // println("Present in Target but not present in target")
+    // df2.except(df1).show(false)
 
   }
+
 }
