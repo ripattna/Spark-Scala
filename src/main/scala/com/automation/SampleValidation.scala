c35933ca38fa43e35f2ffed80e51281a47a40209
ripattna <ripattna@gmail.com>
2021-12-28 20:48:27 +0530

    Code Change

--- src/main/scala/com/automation/SampleValidation.scala
+++ src/main/scala/com/automation/SampleValidation.scala
@@ -5,7 +5,7 @@ import org.apache.spark.sql.functions.{sum, _}
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.types.{LongType, StructField, StructType}
 
-object Validation_Test {
+object SampleValidation {
 
   def main(args: Array[String]): Unit = {
 
@@ -53,7 +53,6 @@ object Validation_Test {
     val join_data = b.join(b1, b("Column_Name") === b1("Column_Name"), "inner").drop(b1("Column_Name")).orderBy("Column_Name")
     //join_data.show()
 
-
     // Inner join to get the Overlap Count
     val valueA_1 = df1.join(df2, Seq("Primary_Key","valueA")).agg(sum("valueA")).select(col("sum(valueA)").as("valueA"))
     val valueB_1 = df1.join(df2, Seq("Primary_Key","valueB")).agg(sum("valueB")).select(col("sum(valueB)").as("valueB"))
@@ -68,19 +67,17 @@ object Validation_Test {
     val join1 = df11.join(df22, Seq("index"))
     val join2 = df33.join(df44, Seq("index"))
     val join_df = join1.join(join2,Seq("index")).drop("index")
-    println("Inner join result:")
-    join_df.show()
+    // println("Inner join result:")
+    //join_df.show()
 
     join_df.createOrReplaceTempView("axis")
     val b01 = spark.sqlContext.sql("select 'valueA' as Column_Name,valueA as Overlap_Count from axis union " +
       "select 'valueB', valueB  from axis union " + "select 'valueC', valueC  from axis union " + "select 'valueD', valueD  from axis")
-    println("Inner join Transpose:")
-    b01.show()
+    // println("Inner join Transpose:")
+    //b01.show()
 
+    // Including the inner join
     val final_df1 = join_data.join(b01,Seq("Column_Name"))
-    println("Including the inner join result:")
-    final_df1.show()
-
 
     val valueA_12 = df1.join(df2, Seq("Primary_Key","valueA"), "leftanti").agg(sum("valueA")).select(col("sum(valueA)").as("valueA"))
     val valueB_12 = df1.join(df2, Seq("Primary_Key","valueB"),"leftanti").agg(sum("valueB")).select(col("sum(valueB)").as("valueB"))
@@ -95,13 +92,13 @@ object Validation_Test {
     val join01 = df011.join(df022, Seq("index"))
     val join02 = df033.join(df044, Seq("index"))
     val join_df0 = join01.join(join02,Seq("index")).drop("index")
-    println("Present in Source not in Target:")
-    join_df0.show()
+    // println("Present in Source not in Target:")
+    // join_df0.show()
 
     join_df0.createOrReplaceTempView("axis")
     val sb01 = spark.sqlContext.sql("select 'valueA' as Column_Name,valueA as Extra_Records_Source_V_1 from axis union " +
       "select 'valueB', valueB  from axis union " + "select 'valueC', valueC  from axis union " + "select 'valueD', valueD  from axis")
-    sb01.show()
+    // sb01.show()
 
     val final_df_s = final_df1.join(sb01,Seq("Column_Name")).orderBy("Column_Name")
 
@@ -118,17 +115,18 @@ object Validation_Test {
     val join901 = df911.join(df922, Seq("index"))
     val join902 = df933.join(df944, Seq("index"))
     val join_df10 = join901.join(join902,Seq("index")).drop("index")
-    println("Present in Source not in Target:")
-    join_df10.show()
+    // println("Present in Source not in Target:")
+    // join_df10.show()
 
     join_df10.createOrReplaceTempView("axis")
     val tb01 = spark.sqlContext.sql("select 'valueA' as Column_Name,valueA as Extra_Records_Target_V_1 from axis union " +
       "select 'valueB', valueB  from axis union " + "select 'valueC', valueC  from axis union " + "select 'valueD', valueD  from axis")
-    tb01.show()
+    // tb01.show()
 
     println("Final Result:")
     val final_df = final_df_s.join(tb01,Seq("Column_Name")).orderBy("Column_Name").na.fill(0)
     final_df.show()
 
   }
+
 }
