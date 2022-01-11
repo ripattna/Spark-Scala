f9e1f42c15db4f48e6326168dd8e4fbdcfdd4393
ripattna <ripattna@gmail.com>
2022-01-11 10:23:10 +0530

    Code Change

---
+++ src/main/scala/com/parquet/ReadParquetFile.scala
@@ -0,0 +1,19 @@
+package com.parquet
+
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.functions.input_file_name
+
+object ReadParquetFile {
+
+  def main(args: Array[String]): Unit = {
+
+    val spark = SparkSession.builder().master("local").appName("ParquetSample").getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val df = spark.read.parquet("src/main/resources/file1.parquet")
+    df.cache()
+    //df.show(false)
+    df.withColumn("file_name", input_file_name()).select("file_name").distinct().show()
+  }
+
+}
