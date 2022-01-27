325663c1e7a96e1ae4efdb024794fd1a41d408f0
ripattna <ripattna@gmail.com>
2022-01-27 17:24:39 +0530

    Code Change

--- src/main/scala/com/automation_2/Test.scala
+++ src/main/scala/com/automation_2/Test.scala
@@ -1,7 +1,7 @@
 package com.automation_2
 
-import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, lit, sum, when}
+import org.apache.spark.sql.{Dataset, Row, SparkSession}
+import org.apache.spark.sql.functions.{col, count, lit, struct, sum, when}
 
 object Test {
 
@@ -14,20 +14,34 @@ object Test {
     val df1 = spark.read.option("header", "true").option("inferSchema", "true").format("csv").load("C:\\Project\\Files\\Input\\File_1.csv")
     df1.show()
 
-    val schemaSchemaList = df1.columns.toList
-    println(schemaSchemaList)
-
     val df2 = spark.read.option("header", "true").option("inferSchema", "true").format("csv").load("C:\\Project\\Files\\Input\\File_2.csv")
     df2.show()
+
+    val schemaSchemaList = df1.columns.toList
+    //println(schemaSchemaList)
 
     val result = df1.join(df2, df1.col("ID") === df2.col("ID"))
       .withColumn("M_Product", when(df1.col("Product") === df2.col("Product"), lit("Y")).otherwise(lit("N")))
       .withColumn("M_Country", when(df1.col("Country") === df2.col("Country"), lit("Y")).otherwise(lit("N")))
       .withColumn("M_Quantity", when(df1.col("Quantity") === df2.col("Quantity"), lit("Y")).otherwise(lit("N")))
       .withColumn("MATCHING", when(col("M_Product") === "Y" && col("M_Country") === "Y" && col("M_Quantity") === "Y", lit("Y")).otherwise(lit("N")))
-
   result.show()
 
-  }
+    println("Matching Records:")
+    val diffDF = df1.intersect(df2)
+    diffDF.show()
 
+    println("Present in Source not in Target:")
+    val inZ1 = df1.exceptAll(df2).toDF()
+    inZ1.show()
+
+    println("Present in Source not in Target:")
+    val inZ = df1.except(df2)
+    inZ.show()
+
+    println("Present in Target not in Source:")
+    val inZ2  = df2.exceptAll(df1).toDF()
+    inZ2.show()
+
+  }
 }
