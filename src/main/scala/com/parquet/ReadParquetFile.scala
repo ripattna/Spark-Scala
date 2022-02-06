4dbbd6d19756126277b34bd39dfdfb73b07d3fe5
ripattna <ripattna@gmail.com>
2022-02-06 09:19:27 +0530

    Code Change

--- src/main/scala/com/parquet/ReadParquetFile.scala
+++ src/main/scala/com/parquet/ReadParquetFile.scala
@@ -8,7 +8,10 @@ object ReadParquetFile {
   def main(args: Array[String]): Unit = {
 
     val spark = SparkSession.builder().master("local").appName("ParquetSample").getOrCreate()
+
     spark.sparkContext.setLogLevel("ERROR")
+
+    val readParquet = spark.read.format("parquet").load("src/main/resources/file1.parquet")
 
     val df = spark.read.parquet("src/main/resources/file1.parquet")
     df.cache()
